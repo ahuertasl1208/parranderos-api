78b3fa4727fc39a1d9a08ad6c3339db245b76e87
ahuertasl1208 <a.huertasl@uniandes.edu.co>
2026-05-11 00:23:35 -0500

    Implementación de endpoints

--- mongo-api.py
+++ mongo-api.py
@@ -30,7 +30,7 @@ def inicio():
 
 @app.get('/api/bares/{bar_id}/comentarios')
 def get_comentarios(bar_id: int):
-    comentarios = None  # TODO: completar
+    comentarios = list(db.comentarios.find({"bar_id": bar_id}, {"_id": 0}))  # TODO: completar
     return comentarios
 
 @app.post('/api/bares/{bar_id}/comentarios')
@@ -38,11 +38,22 @@ def post_comentario(bar_id: int, datos: dict):
     datos['bar_id'] = bar_id
     datos['fecha']  = datetime.now().isoformat()
     # TODO: completar
+    db.comentarios.insert_one(datos)
     return {'mensaje': 'Comentario guardado'}
 
 # TODO: implementar GET /bares/{bar_id}/eventos
 # Debe retornar todos los eventos del bar desde la colección 'eventos'
+@app.get('/api/bares/{bar_id}/eventos')
+def get_eventos(bar_id: int):
+    eventos = list(db.eventos.find({"bar_id": bar_id}, {"_id": 0}))
+    return eventos
 
 # TODO: implementar POST /bares/{bar_id}/eventos  
 # Debe insertar el evento en la colección 'eventos'
 # Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
+@app.post('/api/bares/{bar_id}/eventos')
+def post_evento(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha_creacion'] = datetime.now().isoformat()
+    db.eventos.insert_one(datos)
+    return {'mensaje': 'Evento guardado'}
